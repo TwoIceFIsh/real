7d9d91d93da2423e0826549e06ae8ebed98f31bd
AidenLee <team.k0konutz@gmail.com>
2022-09-15 11:31:38 +0900

    Initial commit

---
+++ src/test/java/today/kokonut/real/RealApplicationTests.java
@@ -0,0 +1,13 @@
+package today.kokonut.real;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
